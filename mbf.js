b3478a4cb0225c682bdf151b5ef7c44efee4ede5
Jeon Younsoo <136571604+msabrin@users.noreply.github.com>
2024-02-15 18:24:22 +0600

    Add files via upload

---
+++ mbf.js
@@ -0,0 +1,29 @@
+$(function(){
+    // Event listener for mouseover on .tricky elements
+    $(".tricky").on({
+        mouseover:function(){
+            $(this).css({
+                left:(Math.random()*90)+"%",
+                top:(Math.random()*90)+"%"
+            });
+        }
+    });
+
+    // Event listener for hover on .btn-wrap elements
+    $(".btn-wrap").hover(function() {
+        $(this).toggleClass('active');
+    });
+
+    // Event listener for touchstart on .tricky elements
+    $('.tricky').bind('touchstart', function(){
+        $(this).css({
+            left:(Math.random()*90)+"%",
+            top:(Math.random()*90)+"%"
+        });
+    });
+
+    // Event listener for click on #yesButton
+    $("#yes").on("click", function() {
+        alert("Yeahh! Now you are mine forever Sam 😘");
+    });
+});
